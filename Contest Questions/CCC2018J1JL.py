db721808486d2067edafef2564d21e40d9970154
Joshua Li <joshua.li@ucc.on.ca>
2018-11-01 12:51:18 -0400

    sample code development added

--- Contest Questions/CCC2018J1JL.py
+++ Contest Questions/CCC2018J1JL.py
@@ -1,3 +1,17 @@
-print("CCC2018J1")
 
-isSpam = (bool)"false";
+isSpam = False;
+first = int(input());
+second = int(input());
+third = int(input());
+last = int(input());
+
+#print(first+" "+second+" "+third+" "+last);
+if first == 8 or first == 9:
+	if second == third:
+		if last == 8 or last == 9:
+			isSpam = True;
+
+if isSpam:
+	print("ignore");
+else:
+	print("answer");
